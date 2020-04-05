adc9dad22f2f99afed0713086641069788a321fb
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-04-04 18:29:35 -0700

    Add regex step matching

--- tests/acceptance/steps/interactions.py
+++ tests/acceptance/steps/interactions.py
@@ -3,7 +3,7 @@ from behave import *
 use_step_matcher('re')
 
 
-@when('I click on the link with id "blog-link"')
-def step_impl(context):
-    link = context.browser.find_element_by_id('blog-link')
+@when('I click on the link with id "(.*)"')
+def step_impl(context, link_id):
+    link = context.browser.find_element_by_id(link_id)
     link.click()
