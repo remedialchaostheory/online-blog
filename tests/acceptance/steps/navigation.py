8c2cfc65f47ac99b7379eec96ee2cc0cb0aa81c5
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-04-05 16:03:04 -0700

    Add "User can create new posts" scenario and respective steps, locator, and page model

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 from tests.acceptance.page_model.blog_page import BlogPage
 from tests.acceptance.page_model.home_page import HomePage
+from tests.acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -18,6 +19,13 @@ def step_impl(context):
 def step_impl(context):
     context.driver = webdriver.Chrome()
     page = BlogPage(context.driver)
+    context.driver.get(page.url)
+
+
+@given('I am on the new post page')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    page = NewPostPage(context.driver)
     context.driver.get(page.url)
 
 
