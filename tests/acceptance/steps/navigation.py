b7694c0bc83ef71519d46dcd7ca1f2fda435d05d
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2020-04-05 13:17:56 -0700

    Refactor urls to page models

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -1,19 +1,24 @@
 from behave import *
 from selenium import webdriver
 
+from tests.acceptance.page_model.blog_page import BlogPage
+from tests.acceptance.page_model.home_page import HomePage
+
 use_step_matcher('re')
 
 
 @given('I am on the homepage')
 def step_impl(context):
     context.driver = webdriver.Chrome()
-    context.driver.get('http://127.0.0.1:5000')
+    page = HomePage(context.driver)
+    context.driver.get(page.url)
 
 
 @given('I am on the blog page')
 def step_impl(context):
     context.driver = webdriver.Chrome()
-    context.driver.get('http://127.0.0.1:5000/blog')
+    page = BlogPage(context.driver)
+    context.driver.get(page.url)
 
 
 @then('I am on the blog page')
